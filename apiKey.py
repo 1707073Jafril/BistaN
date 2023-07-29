4dad5a58bc3583d65dd845c650eb69c30d61e7a8
Md. Jafril Alam <82281358+1707073Jafril@users.noreply.github.com>
2023-07-30 01:55:46 +0600

    Add files via upload

---
+++ apiKey.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 22 04:56:46 2023
+
+@author: shbmsk
+"""
+
+openapi_key = "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"
+serpapi_key = "05e0c542a45e02dd44b9c6ad6185796d95dbe9a83a8804c8a9c945db2462d9b4"
